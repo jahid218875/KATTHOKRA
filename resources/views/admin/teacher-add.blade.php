f0b30970854c761c0b61fed6ee6cd02061f9f804
jahid218875 <jahid218875@gmail.com>
2022-09-05 17:01:02 +0600

    up

---
+++ resources/views/admin/teacher-add.blade.php
@@ -0,0 +1,144 @@
+@extends('admin.layouts.app')
+
+@section('content')
+<div id="main">
+    <header class="mb-3">
+        <a href="#" class="burger-btn d-block d-xl-none">
+            <i class="bi bi-justify fs-3"></i>
+        </a>
+    </header>
+
+    <div class="page-heading">
+        {{-- <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Add Subject</h3>
+                    <p class="text-subtitle text-muted">Multiple form layouts, you can use</p>
+                </div>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Form Layout</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div> --}}
+
+        <!-- // Basic multiple Column Form section start -->
+
+        <section id="multiple-column-form">
+            <div class="row match-height">
+                @if(session('success'))
+                <script>
+                    Swal.fire(
+                    'Good job!',
+                    '{{ session('success') }}',
+                    'success'
+                    )
+                </script>
+                @endif
+
+                @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul class="list-unstyled">
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+
+
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Add New Editor</h4>
+                        </div>
+                        <div class="card-content">
+                            <div class="card-body">
+                                <form class="form" action="{{ route('admin.editor_process')}}" method="post"
+                                    enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="form-group">
+                                                <label for="name">Editor Name</label>
+                                                <input type="text" id="name" class="form-control"
+                                                    placeholder="Editor Name" name="name" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-group">
+                                                <label for="email">Email</label>
+                                                <input type="email" id="email" class="form-control"
+                                                    placeholder="Enter Editor Email" name="email" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-group">
+                                                <label for="mobile">Mobile Number</label>
+                                                <input type="text" id="mobile" class="form-control"
+                                                    placeholder="Enter Editor Mobile Number" name="mobile" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-group">
+                                                <label for="password">Password</label>
+                                                <input type="password" id="password" class="form-control"
+                                                    placeholder="Enter Editor Email" name="password" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12 d-flex justify-content-end">
+                                            <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- // Basic multiple Column Form section end -->
+
+        <!-- Basic Tables start -->
+        {{-- <section class="section">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table" id="table1">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Mobile</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($editor as $editor_list)
+                            <tr>
+                                <td>{{$editor_list->name}}</td>
+                                <td>{{$editor_list->email}}</td>
+                                <td>{{$editor_list->mobile}}</td>
+                                <td>
+                                    <a href="{{ route('admin.editor_delete', $editor_list->id)}}"
+                                        onclick="return confirm('are you sure?')" class="badge bg-danger">Delete</a>
+                                </td>
+                            </tr>
+                            @endforeach
+
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </section> --}}
+        <!-- Basic Tables end -->
+    </div>
+</div>
+
+@endsection
